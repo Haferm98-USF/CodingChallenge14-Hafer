1c9d58410d07d356eece324b55dc42450e603993
Haferm98 <105403350+Haferm98@users.noreply.github.com>
2024-11-03 15:27:04 -0500

    Dynamically display tickets on the page.

--- main.js
+++ main.js
@@ -11,4 +11,29 @@ async function displayUnresolvedTickets(id)
     }
     const ticketId = await ticketResponse.json();
     console.log('Ticket ID: ', ticketId);
+
+//Task 3: Display Tickets Dynamically on the Page
+
+const apiUrl = 'https://jsonplaceholder.typicode.com/posts';
+try {
+    fetch(apiUrl)
+    .then(response => response.json())
+    .then(tickets => 
+
+    {
+        tickets.forEach(ticket =>
+        {
+            const listItem = document.createElement('li');
+            listItem.textContent = 'Ticket ID: ${ticket.id}, Customer Name: ${ticket.userId}, Issue: ${ticket.title}, Details: ${ticket.body}';
+            ticketList.appendChild(listItem);
+        }
+        );
+    }
+    )}
+
+catch (error) 
+{
+    console.error('Error: Something went Wrong')
+}
+
 }
